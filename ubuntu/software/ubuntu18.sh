65457576ba5a0cc556b6a2aefb2d3d99bcfb506f
Your Name <you@example.com>
2024-07-19 16:33:33 +0800

    software

---
+++ ubuntu/software/ubuntu18.sh
@@ -0,0 +1,15 @@
+
+sudo apt update
+sudo apt upgrade -y
+#裁剪版
+sudo add-apt-repository ppa:hluk/copyq   -y
+sudo apt update
+sudo apt install copyq   -y
+#深度截图
+sudo apt install deepin-screenshot  -y
+
+#screen 录屏软件
+sudo apt install deepin-screenshot  -y
+
+#vscode 
+snap install code -y
