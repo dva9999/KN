e7a7a6602bcc5fe42dec471f7f0f0735961b3b36
Your Name <you@example.com>
2024-08-29 08:48:59 +0800

    modified:   ubuntu/software/ubuntu18.sh

--- ubuntu/software/ubuntu18.sh
+++ ubuntu/software/ubuntu18.sh
@@ -1,4 +1,7 @@
 
+
+sudo 
+
 sudo apt update
 sudo apt upgrade -y
 #裁剪版
@@ -20,3 +23,32 @@ sudo apt-get install gnome-tweaks gnome-tweak-tool -y
 
 
 sudo apt-get install filezilla -y
+
+sudo add-apt-repository -y ppa:teejee2008/timeshift
+sudo apt-get update
+sudo apt-get install timeshift -y 
+
+sudo apt install  docker.io  -y
+
+
+wget https://ime-sec.gtimg.com/202408290834/d59c74a2e07b8749f5bab265f53e20ae/pc/dl/gzindex/1680521603/sogoupinyin_4.2.1.145_amd64.deb 
+
+sudo apt install ./sogoupinyin_4.2.1.145_amd64.deb -y
+
+sudo apt-get install fcitx -y
+
+sudo apt -f install -y
+
+
+ sudo apt purge ibus -y 
+
+sudo apt install libqt5qml5 libqt5quick5 libqt5quickwidgets5 qml-module-qtquick2  -y
+
+sudo apt install libgsettings-qt1 -y 
+
+
+
+
+
+
+
